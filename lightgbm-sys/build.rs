dd1a56a7bb44b110ce8effe7ce6ff75502dc8500
Nico Mandery <nico@nmandery.net>
2022-04-12 15:36:46 +0200

    upgrade to lightgbm 3.3.2 directly from Microsoft repo + smaller fixes

--- lightgbm-sys/build.rs
+++ lightgbm-sys/build.rs
@@ -40,12 +40,13 @@ fn main() {
         .profile("Release")
         .uses_cxx11()
         .define("BUILD_STATIC_LIB", "ON")
+        .define("USE_OPENMP", "ON")
         .build();
 
     // bindgen build
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_args(&["-x", "c++", "-std=c++11"])
+        .clang_args(&["-x", "c"])
         .clang_arg(format!("-I{}", lgbm_root.join("include").display()))
         .generate()
         .expect("Unable to generate bindings");
